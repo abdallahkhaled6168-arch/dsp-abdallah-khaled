507785cbfeabd9b42262e08e522790f421bf7bb3
abdallahkhaled6168-arch <abdallahkhaled6168@gmail.com>
2026-05-01 15:02:48 +0300

    Update train.py

--- house_prices/train.py
+++ house_prices/train.py
@@ -1,27 +1,50 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 import joblib
-from house_prices.preprocess import preprocess_data
+
 
 def build_model():
     df = pd.read_csv("data/train.csv", sep="\s+")
 
-    df = preprocess_data(df)
-
     X = df.drop("SalePrice", axis=1)
     y = df["SalePrice"]
+
+    numeric_cols = X.select_dtypes(include=["int64", "float64"]).columns
+    categorical_cols = X.select_dtypes(include=["object"]).columns
+
+    numeric_pipeline = Pipeline([
+        ("scaler", StandardScaler())
+    ])
+
+    categorical_pipeline = Pipeline([
+        ("encoder", OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+    preprocessor = ColumnTransformer([
+        ("num", numeric_pipeline, numeric_cols),
+        ("cat", categorical_pipeline, categorical_cols)
+    ])
+
+    model = Pipeline([
+        ("preprocessor", preprocessor),
+        ("regressor", LinearRegression())
+    ])
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    model = LinearRegression()
+    print("Training model...")
+
     model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
+    preds = model.predict(X_test)
+    mae = mean_absolute_error(y_test, preds)
 
     joblib.dump(model, "models/model.joblib")
 
