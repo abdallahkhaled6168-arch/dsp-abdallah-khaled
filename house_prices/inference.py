7be2535e3f05f947fff4bee1c19728d91335b346
abdallahkhaled6168-arch <abdallahkhaled6168@gmail.com>
2026-05-01 15:03:51 +0300

    Update inference.py

--- house_prices/inference.py
+++ house_prices/inference.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import joblib
-from house_prices.preprocess import preprocess_data
+
 
 def make_predictions():
     model = joblib.load("models/model.joblib")
@@ -8,18 +8,13 @@ def make_predictions():
     df = pd.read_csv("data/test.csv", sep="\s+")
     ids = df["Id"]
 
-    df = preprocess_data(df)
-
-    if "SalePrice" in df.columns:
-        df = df.drop("SalePrice", axis=1)
-
-    predictions = model.predict(df)
+    preds = model.predict(df)
 
     output = pd.DataFrame({
         "Id": ids,
-        "SalePrice": predictions
+        "SalePrice": preds
     })
 
     output.to_csv("predictions.csv", index=False)
 
-    return "Predictions saved!"
+    return "Predictions saved"
