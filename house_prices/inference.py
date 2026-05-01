94904685b5bb8ca9cbc178b740df1169e5cc8bd4
abdallahkhaled6168-arch <abdallahkhaled6168@gmail.com>
2026-05-01 17:40:23 +0300

    Update inference.py

--- house_prices/inference.py
+++ house_prices/inference.py
@@ -2,31 +2,30 @@ import pandas as pd
 import joblib
 
 def make_predictions():
+    df = pd.read_csv("data/test.csv", sep="\t")
+    df.columns = df.columns.str.strip()
+
+    if "Id" in df.columns:
+        ids = df["Id"]
+        df = df.drop(columns=["Id"])
+    else:
+        ids = range(len(df))
+
+    X = pd.get_dummies(df)
+    X = X.fillna(0)
+
     model = joblib.load("models/model.pkl")
 
-    train_df = pd.read_csv("data/train.csv")
-    test_df = pd.read_csv("data/test.csv")
+    model_features = model.feature_names_in_
+    X = X.reindex(columns=model_features, fill_value=0)
 
-    y = train_df["SalePrice"]
-    train_df = train_df.drop(columns=["SalePrice"])
-
-    train_df = train_df.drop(columns=["Id"], errors="ignore")
-    test_ids = test_df["Id"]
-    test_df = test_df.drop(columns=["Id"], errors="ignore")
-
-    train_df = train_df.fillna(0)
-    test_df = test_df.fillna(0)
-
-    train_df = pd.get_dummies(train_df)
-    test_df = pd.get_dummies(test_df)
-
-    test_df = test_df.reindex(columns=train_df.columns, fill_value=0)
-
-    preds = model.predict(test_df)
+    preds = model.predict(X)
 
     output = pd.DataFrame({
-        "Id": test_ids,
+        "Id": ids,
         "SalePrice": preds
     })
 
     output.to_csv("predictions.csv", index=False)
+
+    print("Predictions file created successfully")
